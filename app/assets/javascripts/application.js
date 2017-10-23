acd5f172a715355fb17a2bb1f0631602645e00c7
MargaritaKhodzka <mkhodzka@gmail.com>
2017-10-22 20:07:33 -0400

    added categories.js, set up categories#show view and controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require destinations.js
+// require destinations
+// require categories
---
+++ app/assets/javascripts/categories.js
@@ -0,0 +1,8 @@
+'use strict'
+
+$(() => {
+  let path = $(location).attr('href')
+  $.getJSON(path, function (data) {
+    console.log(data)
+  })
+}) 
