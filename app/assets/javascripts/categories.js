19cf1d2ed605a91ee0d313ab35226e132696a7d2
MargaritaKhodzka <mkhodzka@gmail.com>
2017-10-24 15:48:59 -0400

    added next category button to the show page

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,8 +1,27 @@
 'use strict'
 
+let categoriesValues
+
 $(() => {
-  let path = $(location).attr('href')
-  $.getJSON(path, function (data) {
-    console.log(data)
+  $.getJSON('/items.json', function (data) {
+    categoriesValues = $.map(data, function (e) {
+      return e.id
+    })
   })
-}) 
+})
+
+$('js-next').on('click', function () {
+  let nextIndex
+  let dataIdIndex = categoriesValues.indexOf(parseInt($('.js-next').attr('data-id')))
+  if (dataIdIndex === categoriesValues.length - 1)
+    nextIndex = 0
+  else
+    nextIndex - dataIdIndex + 1
+
+  $.getJSON('/categories/' + categoriesValues[nextIndex], function(data) {
+    $('#title').html(`${data['title']}`)
+    $('#climate').html(`Climate: ${data['climate']}`)
+    $('#must_have_items').html(`Must Have Items: ${data['must_have_items']}`)
+    $('.js-next').attr('data-id', data['id'])
+  })
+})
