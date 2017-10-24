6bd313340db3f308b88511acd4c851d90751943f
MargaritaKhodzka <mkhodzka@gmail.com>
2017-10-24 15:21:28 -0400

    added json endpoint to categories controller#index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,10 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = current_user.try(:categories)
+    respond_to do |f|
+      f.html { render :index }
+      f.json { render json: @categories }
+    end
   end
 
   def new
