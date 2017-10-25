390d6c817d667af07b1679846f00e479a954b38f
MargaritaKhodzka <mkhodzka@gmail.com>
2017-10-25 01:20:31 -0400

    added next destination button

--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -12,7 +12,31 @@ $(() => {
         })
         $('#categories').html(categoryList)
      } else {
-       $('#categories').html('<div><h4>You do not have any categories_attributes yet.</h4></div>')
+       $('#categories').html('<div><h4>You do not have any categories yet.</h4></div>')
      }
+  })
+  $.getJSON('/destinations.json', function (data) {
+    destinationValues = $.map(data, function (e) {
+      return e.id
+    })
+  })
+})
+$(function () {
+  $('.js-next').on('click', function () {
+    let nextIndex
+    let dataIdIndex = destinationValues.indexOf(parseInt($('.js-next').attr('data-id')))
+    if (dataIdIndex === destinationValues.length - 1)
+      nextIndex = 0
+    else
+      nextIndex = dataIdIndex + 1
+
+    $.get('/destinations/' + destinationValues[nextIndex], function (data) {
+      $('#name').html(`${data['name']}`)
+      $('#description').html(`${data['description']}`)
+      $('#country').html(`${data['country']}`)
+      $('#best_season_to_visit').html(`${data['best_season_to_visit']}`)
+      $('#visited').html(`${data['visited']}`)
+      $('.js-next').attr('data-id', data['id'])
+    })
   })
 })
