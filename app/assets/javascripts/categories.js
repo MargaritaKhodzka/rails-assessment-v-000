d170959efa9e115623db43028d6a4b51e3772705
MargaritaKhodzka <mkhodzka@gmail.com>
2017-10-25 15:05:10 -0400

    edited categories and destinations js

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,6 +1,33 @@
+'use strict'
+
 $(() => {
-  let path = $(location).attr('href')
-  $.getJSON(path, function (data) {
-    console.log(data)
+  $.getJSON('/categories.json', function (data) {
+    let categoriesValues = $.map(data, function (e) {
+      return e.id
+    })
   })
-}) 
+  $('form').submit(function (e) {
+    e.preventDefault()
+    let values = $(this).serialize()
+    let posting = $.post('/categories', values)
+    posting.done(function (data) {
+      $('#title').html(`${data['title']}`)
+      $('#climate').html(`Rating: ${data['climate']}`)
+      $('#must_have_items').html(`Notes: ${data['must_have_items']}`)
+    })
+  })
+})
+
+$('.js-next').on('click', function () {
+  let nextIndex
+  let dataIdIndex = categoriesValues.indexOf(parseInt($('.js-next').attr('data-id')))
+  if (dataIdIndex === categoriesValues.length - 1)
+    nextIndex = 0
+  else
+    nextIndex = dataIdIndex + 1
+  $.getJSON('/categories/' + categoriesValues[nextIndex], function (data) {
+    $('#title').html(`${data['title']}`)
+    $('#climate').html(`Rating: ${data['climate']}`)
+    $('#must_have_items').html(`Notes: ${data['must_have_items']}`)
+  })
+})
--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,42 +1,20 @@
-$(() => {
-   let path = $(location).attr('href')
-   $.get(path, function (data) {
-     if (data.categories.length > 0) {
-       let categoryList = $()
-       data.categories.forEach(function (category) {
-         categoryList = categoryList.add(`<li><strong><a href='/categories/${category['id']}'>${category['title']}</a></strong>
-         <ul>
-            <li>Climate: ${category['climate']}</li>
-            <li>Must Have Item: ${category['must_have_items']}</li>
-          </ul>`)
-        })
-        $('#categories').html(categoryList)
-     } else {
-       $('#categories').html('<div><h4>You do not have any categories yet.</h4></div>')
-     }
-  })
-  $.getJSON('/destinations.json', function (data) {
-    destinationValues = $.map(data, function (e) {
-      return e.id
-    })
-  })
-})
-$(function () {
-  $('.js-next').on('click', function () {
-    let nextIndex
-    let dataIdIndex = destinationValues.indexOf(parseInt($('.js-next').attr('data-id')))
-    if (dataIdIndex === destinationValues.length - 1)
-      nextIndex = 0
-    else
-      nextIndex = dataIdIndex + 1
+'use strict'
 
-    $.get('/destinations/' + destinationValues[nextIndex], function (data) {
-      $('#name').html(`${data['name']}`)
-      $('#description').html(`${data['description']}`)
-      $('#country').html(`${data['country']}`)
-      $('#best_season_to_visit').html(`${data['best_season_to_visit']}`)
-      $('#visited').html(`${data['visited']}`)
-      $('.js-next').attr('data-id', data['id'])
-    })
+$(() => {
+  let path = $(location).attr('href')
+  $.getJSON(path, function (data) {
+    if (data.categories.length > 0) {
+      let categoryList = $()
+      data.categories.forEach(function (category) {
+        categoryList = categoryList.add(`<li><strong><a href='/categories/${category['id']}'>${category['title']}</a></strong>
+            <ul>
+                <li>Climate: ${category['climate']}</li>
+                <li>Must Have Items: ${category['must_have_items']}</li>
+            </ul>`)
+      })
+      $('#categories').html(categoryList)
+    } else {
+      $('#categories').html('<div><h3>You do not have any categories yet.</h3></div>')
+    }
   })
 })
