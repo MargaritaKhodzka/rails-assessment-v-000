be0e5515682c6ca79083d71fbc1ce3b6ae6ca86c
MargaritaKhodzka <mkhodzka@gmail.com>
2017-10-25 00:10:55 -0400

    destinations serializer

---
+++ app/assets/javascripts/destinations.js
@@ -0,0 +1,5 @@
+$(function () {
+  $('li').on('click', function (e) {
+    console.log(e)
+  })
+})
--- db/migrate/20171008013249_drop_users_destinations.rb
+++
@@ -1,5 +0,0 @@
-class DropUsersDestinations < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :users_destinations
-  end
-end
