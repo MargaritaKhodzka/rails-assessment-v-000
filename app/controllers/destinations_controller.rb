9b103b8a08e734f6cb32f4e0287f07ad3dec2496
MargaritaKhodzka <mkhodzka@gmail.com>
2017-11-01 16:24:20 -0400

    updated destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,67 +1,62 @@
 class DestinationsController < ApplicationController
   before_action :authenticate_user!
   before_action :all_categories, only: %i[new create edit update show]
-  before_action :current_destination, only: %i[update destroy]
+  before_action :current_destination, only: %i[show edit update destroy]
 
   def index
-    @destinations = current_user.destinations
+    @destinations = Destination.all.order('created_at ASC')
     respond_to do |format|
-      format.html { render :index }
+      format.html
       format.json { render json: @destinations }
     end
   end
 
   def new
-    @destination = Destination.new(user_id: current_user.id)
-    @destination.categories.build
+    @destination = Destination.new
+    @categories = Array.new { @destination.categories.build }
   end
 
   def create
     @destination = Destination.new(destination_params)
+    @destination = current_user.destinations.build(destination_params)
     if @destination.save
-      render json: @destination, status: 201
+      redirect_to destination_path(@destination), notice: 'A new destination has been created'
     else
       render :new
     end
   end
 
   def show
-    if current_destination
-      @categories = @destination.categories
-      @category = Category.new
-      respond_to do |format|
-        format.html { render :show }
-        format.json { render json: @destination }
-      end
-    else
-      redirect_to destinations_path
+    @destination = Destination.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @destination }
     end
   end
 
   def edit
-    if current_destination
-      @destination.categories.build
-      render :edit
-    else
-      redirect_to destinations_path
-    end
+
   end
 
   def update
     if @destination.update(destination_params)
-      redirect_to @destination
+      redirect_to @destination, notice: 'Your destination has been successfully updated'
     else
       render :edit
     end
   end
 
+  def visited(_user)
+    @visits << Visited.new(user: @user)
+  end
+
+  def notvisited(_user)
+    @visits.where(user_id: @user.id).first.destroy
+  end
+
   def destroy
-    name = @destination.name
-    if @destination.delete
-      redirect_to destinations_path, notice: "#{name} deleted successfully."
-    else
-      render :show
-    end
+    @destination.destroy
+    redirect_to root_path, notice: 'You have deleted this destination'
   end
 
   private
--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  before_action :authenticate_user!, only: [:visited]
-
-  def home
-  end
-
-end
